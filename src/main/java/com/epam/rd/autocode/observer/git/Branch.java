45902180222a367aeb2d2009f5ece53918e2c51e
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2022-03-04 16:09:23 +0300

    Initial Commit

---
+++ src/main/java/com/epam/rd/autocode/observer/git/Branch.java
@@ -0,0 +1,31 @@
+package com.epam.rd.autocode.observer.git;
+
+import java.util.Objects;
+
+public class Branch {
+
+    private final String name;
+
+    public Branch(final String name) {
+        Objects.requireNonNull(name);
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Branch branch = (Branch) o;
+        return name.equals(branch.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
